2f6549ea25461e2c572af2299a5a083369cc3392
homxuwang <wanghongxu1994@qq.com>
2022-08-07 11:00:16 +0800

    修复了热力图在博客中实现时，点击事件无法作用的bug

--- myjs/initheatmap.js
+++ myjs/initheatmap.js
@@ -1,32 +1,52 @@
+
 window.onload = function() {
-    var hm = document.body
-      var heatmap = h337.create({
-          container: hm,
-          radius: 60
-      });
-      var trackData = false;
-      setInterval(function() {
-      trackData = true;
-      }, 50);
-      var idleTimeout, idleInterval;
-      var lastX, lastY;
-      var idleCount;
-      function startIdle() {
-      idleCount = 0;
-      function idle() {
-          heatmap.addData({
-          x: lastX,
-          y: lastY
-          });
-          idleCount++;
-          if (idleCount > 10) {
-          clearInterval(idleInterval);
-          }
-      };
-      idle();
-      idleInterval = setInterval(idle, 1000);
-      };
-      document.body.onmousemove = function(ev) {
+  var body = document.body
+  var bodyStyle = getComputedStyle(body);
+
+  var heatmapDIV = document.createElement('div')
+  heatmapDIV.style.position= 'fixed'
+  heatmapDIV.style.width = bodyStyle.width
+  heatmapDIV.style.height = bodyStyle.height
+  heatmapDIV.classList.add('heatmap')
+  
+  let firstChild = document.body.firstChild
+  body.insertBefore(heatmapDIV, firstChild)
+
+  // hm.appendChild(heatmapWrapper)
+  // var hmEl = document.querySelector('.heatmap-wrapper');
+  // hmEl.style.width = bodyStyle.width;
+  // hmEl.style.height = bodyStyle.height;
+  var hm = document.querySelector('.heatmap');
+  var heatmap = h337.create({
+      container: hm,
+      radius: 60
+  });
+  if(hm.style.position == 'relative'){
+    hm.style.position = 'absolute'
+  }
+    var trackData = false;
+    setInterval(function() {
+    trackData = true;
+    }, 50);
+    var idleTimeout, idleInterval;
+    var lastX, lastY;
+    var idleCount;
+    function startIdle() {
+    idleCount = 0;
+    function idle() {
+        heatmap.addData({
+        x: lastX,
+        y: lastY
+        });
+        idleCount++;
+        if (idleCount > 10) {
+        clearInterval(idleInterval);
+        }
+    };
+    idle();
+    idleInterval = setInterval(idle, 1000);
+    };
+    hm.onmousemove = function(ev) {
       if (idleTimeout) clearTimeout(idleTimeout);
       if (idleInterval) clearInterval(idleInterval);
       if (trackData) {
@@ -39,5 +59,5 @@ window.onload = function() {
           trackData = false;
       }
       idleTimeout = setTimeout(startIdle, 500);
-      };
-    }
+    };
+  }
